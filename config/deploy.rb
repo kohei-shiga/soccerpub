2b3aeef107063fd9e59c8c9cfa6704c0b52d3d61
kohei shiga <iniestasoccer.38201022@i.softbank.jp>
2021-02-09 03:52:19 +0000

    reamend deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/var/www/rails/soccerpub'
 
 set :branch, 'main'
 
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :keep_releases, 5
