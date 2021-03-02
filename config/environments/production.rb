2112ad8b9ef435c9ce3bf5d742a01a140d6d481e
シガコウヘイ <iniestasoccer.38201022@i.softbank.jp>
2021-03-02 16:00:16 +0900

    Add action_mailer.default_url_options to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,16 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendgrid
-  # host = 'soccerpub-88.herokuapp.com'
-  # config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   port: ENV['MAILGUN_SMTP_PORT'],
-  #   address: ENV['MAILGUN_SMTP_SERVER'],
-  #   user_name: ENV['MAILGUN_SMTP_LOGIN'],
-  #   password: ENV['MAILGUN_SMTP_PASSWORD'],
-  #   domain: host,
-  #   authentication: :plain
-  # }
+  host = 'www.soccerpub-app.work'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
